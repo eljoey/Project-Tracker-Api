b7fbb66c8836713a23b35ad53143b42f14f38cbf
Joe Hayden <eljoeyy@gmail.com>
2020-01-31 11:24:26 -0600

    project model

---
+++ models/project.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const projectSchema = new Schema({
+  name: {
+    type: Schema,
+    required: true
+  },
+  admin: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  created: {
+    type: Date,
+    required: true,
+    default: Date.now()
+  },
+  bugs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Bug'
+    }
+  ],
+  features: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Feature'
+    }
+  ]
+})
