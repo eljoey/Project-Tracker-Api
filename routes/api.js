a128e8e0bf73666d6cf24956ddebaed3a69cbcc4
Joe Hayden <eljoeyy@gmail.com>
2020-02-03 13:56:43 -0600

    adjust routes

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,4 @@ router.post('/user/create', userRouter.user_create_post)
 
 router.get('/user/:id', userRouter.user_id_get)
 
-router.get('/user/:username/projects', userRouter.projects_get)
-
 module.exports = router
